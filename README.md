353b6e0f464460673a5ac1591a65984c5e5972a9
santiago <santiagobarragan@gamil.com>
2024-05-29 21:44:30 -0500

    hl

--- README.md
+++ README.md
@@ -1,1 +1,114 @@
-[![Review Assignment Due Date](https://classroom.github.com/assets/deadline-readme-button-24ddc0f5d75046c5622901739e7c5dd533143b0c8e959d652212380cedb1ea36.svg)](https://classroom.github.com/a/-Z2PGDOu)
+import java.util.ArrayList;
+import java.util.List;
+
+
+enum TipoPokemon {
+    FUEGO, AGUA, PLANTA, ELECTRICO, NORMAL;
+
+   
+    public double obtenerMultiplicadorDeDaño(TipoPokemon atacante, TipoPokemon defensor) {
+       
+        return 1.0;
+    }
+}
+
+
+abstract class Pokemon {
+    protected String nombre;
+    protected double salud;
+    protected double puntosDeAtaque;
+    protected TipoPokemon tipo;
+    protected Estado estado;
+
+    
+    public Pokemon(String nombre, double salud, double puntosDeAtaque, TipoPokemon tipo) {
+        this.nombre = nombre;
+        this.salud = salud;
+        this.puntosDeAtaque = puntosDeAtaque;
+        this.tipo = tipo;
+        this.estado = Estado.NORMAL;
+    }
+
+    public void atacar(Pokemon oponente) {
+        double multiplicadorDeDaño = this.tipo.obtenerMultiplicadorDeDaño(this.tipo, oponente.tipo);
+        double dañoInfligido = this.puntosDeAtaque * multiplicadorDeDaño;
+        oponente.recibirDaño(dañoInfligido);
+    }
+
+    public void recibirDaño(double daño) {
+        this.salud -= daño;
+        if (this.salud <= 0) {
+            this.estado = Estado.DEBILITADO;
+        }
+    }
+
+    
+    public void entrenar() {
+       
+    }
+}
+
+
+class Entrenador {
+    private String nombre;
+    private List<Pokemon> equipo;
+    private List<Pokemon> pokemones;
+
+    public Entrenador(String nombre) {
+        this.nombre = nombre;
+        this.equipo = new ArrayList<>();
+        this.pokemones = new ArrayList<>();
+    }
+
+   
+    public void agregarPokemon(Pokemon pokemon) {
+        this.equipo.add(pokemon);
+        this.pokemones.add(pokemon);
+    }
+
+    
+    public void entrenarPokemon(Pokemon pokemon) {
+        pokemon.entrenar();
+    }
+
+    
+    public void mostrarPokemones() {
+        for (Pokemon pokemon : pokemones) {
+            System.out.println(pokemon.nombre);
+        }
+    }
+
+    public Pokemon prepararBatalla() {
+        
+        return equipo.get(0); 
+    }
+
+    public void batallar(Entrenador oponente) {
+    }
+}
+
+enum Estado {
+    NORMAL, DEBILITADO
+}
+
+class Batalla {
+    public void iniciarBatalla(Pokemon pokemon1, Pokemon pokemon2) {
+        
+    }
+}
+
+class Growlithe extends Pokemon {
+    public Growlithe(String nombre, double salud, double puntosDeAtaque) {
+        super(nombre, salud, puntosDeAtaque, TipoPokemon.FUEGO);
+    }
+
+    
+}
+
+class Onix extends Pokemon {
+    public Onix(String nombre, double salud, double puntosDeAtaque) {
+        super(nombre, salud, puntosDeAtaque, TipoPokemon.TIERRA);
+    }
+
+    
+}
--- src/main/java/simulador/Principal.java
+++ src/main/java/simulador/Principal.java
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 package simulador;
 import java.util.HashMap;
 import java.util.Map;
@@ -63,5 +64,119 @@ public class Principal {
         System.out.println("Salud: " + pokemon.getSalud());
         System.out.println("Ataque: " + pokemon.getAtaque());
         System.out.println("--------------------------");
+=======
+import java.util.ArrayList;
+import java.util.List;
+
+
+enum TipoPokemon {
+    FUEGO, AGUA, PLANTA, ELECTRICO, NORMAL;
+
+   
+    public double obtenerMultiplicadorDeDaño(TipoPokemon atacante, TipoPokemon defensor) {
+       
+        return 1.0;
     }
+}
+
+
+abstract class Pokemon {
+    protected String nombre;
+    protected double salud;
+    protected double puntosDeAtaque;
+    protected TipoPokemon tipo;
+    protected Estado estado;
+
+    
+    public Pokemon(String nombre, double salud, double puntosDeAtaque, TipoPokemon tipo) {
+        this.nombre = nombre;
+        this.salud = salud;
+        this.puntosDeAtaque = puntosDeAtaque;
+        this.tipo = tipo;
+        this.estado = Estado.NORMAL;
+    }
+
+    public void atacar(Pokemon oponente) {
+        double multiplicadorDeDaño = this.tipo.obtenerMultiplicadorDeDaño(this.tipo, oponente.tipo);
+        double dañoInfligido = this.puntosDeAtaque * multiplicadorDeDaño;
+        oponente.recibirDaño(dañoInfligido);
+    }
+
+    public void recibirDaño(double daño) {
+        this.salud -= daño;
+        if (this.salud <= 0) {
+            this.estado = Estado.DEBILITADO;
+        }
+>>>>>>> Stashed changes
+    }
+
+    
+    public void entrenar() {
+       
+    }
+}
+
+
+class Entrenador {
+    private String nombre;
+    private List<Pokemon> equipo;
+    private List<Pokemon> pokemones;
+
+    public Entrenador(String nombre) {
+        this.nombre = nombre;
+        this.equipo = new ArrayList<>();
+        this.pokemones = new ArrayList<>();
+    }
+
+   
+    public void agregarPokemon(Pokemon pokemon) {
+        this.equipo.add(pokemon);
+        this.pokemones.add(pokemon);
+    }
+
+    
+    public void entrenarPokemon(Pokemon pokemon) {
+        pokemon.entrenar();
+    }
+
+    
+    public void mostrarPokemones() {
+        for (Pokemon pokemon : pokemones) {
+            System.out.println(pokemon.nombre);
+        }
+    }
+
+    public Pokemon prepararBatalla() {
+        
+        return equipo.get(0); 
+    }
+
+    public void batallar(Entrenador oponente) {
+    }
+}
+
+enum Estado {
+    NORMAL, DEBILITADO
+}
+
+class Batalla {
+    public void iniciarBatalla(Pokemon pokemon1, Pokemon pokemon2) {
+        
+    }
+}
+
+class Growlithe extends Pokemon {
+    public Growlithe(String nombre, double salud, double puntosDeAtaque) {
+        super(nombre, salud, puntosDeAtaque, TipoPokemon.FUEGO);
+    }
+
+    
+}
+
+class Onix extends Pokemon {
+    public Onix(String nombre, double salud, double puntosDeAtaque) {
+        super(nombre, salud, puntosDeAtaque, TipoPokemon.TIERRA);
+    }
+
+    
 }
